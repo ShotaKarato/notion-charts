b72a6aa97d8b0d93db7d536035116e19382a7d4c
Shota Karato <theseuta@gmail.com>
2023-03-19 00:29:56 +0900

    :hammer: generate Charts with QuickCharts
    generate Charts with QuickCharts

--- index.ts
+++ index.ts
@@ -1,4 +1,11 @@
 import { Client } from "@notionhq/client";
+import { DateTime } from "luxon";
+import QuickChart from "quickchart-js";
+
+import type {
+  PageObjectResponse,
+  PartialPageObjectResponse,
+} from "@notionhq/client/build/src/api-endpoints";
 
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
 
@@ -15,10 +22,55 @@ const queryDailyRetrospects = async () => {
         },
       },
     });
-    console.log(result);
+    const { thisMonthData, lastMonthData } = getTargetRangeData(result.results);
+
+    const myChart = new QuickChart();
+    myChart
+      .setConfig({
+        type: "bar",
+        data: {
+          labels: ["Last Month", "This Month"],
+          datasets: [
+            {
+              label: "Daily Retrospects",
+              data: [lastMonthData.data, thisMonthData.data],
+            },
+          ],
+        },
+      })
+      .setWidth(500)
+      .setHeight(300);
+
+    console.log(myChart.getUrl());
   } catch (error) {
     console.log(error);
   }
+};
+
+const getTargetRangeData = (
+  data: (PageObjectResponse | PartialPageObjectResponse)[]
+) => {
+  const thisMonthData = data.filter(
+    (dailyRetrospect) =>
+      DateTime.fromISO(dailyRetrospect.created_time).month ===
+      DateTime.now().month
+  );
+  const lastMonthData = data.filter(
+    (dailyRetrospect) =>
+      DateTime.fromISO(dailyRetrospect.created_time).month ===
+      DateTime.now().month - 1
+  );
+  console.log();
+  return {
+    thisMonthData: {
+      month: DateTime.now().month,
+      data: thisMonthData.length,
+    },
+    lastMonthData: {
+      month: DateTime.now().month - 1,
+      data: lastMonthData.length,
+    },
+  };
 };
 
 queryDailyRetrospects();
